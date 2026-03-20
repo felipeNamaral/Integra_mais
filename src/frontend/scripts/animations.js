f6be4fa19013c50850b7eda9c2a846607e2f6196
Felipe Amaral <fe.nataliamaral@gmail.com>
2026-03-20 16:01:21 -0300

    feat(ui): implementa telas de index e login com estrutura inicial
    * cria tela principal (index)
    * implementa tela de login
    * adiciona arquivos base (main.css e variables.css)
    * aplica estilos globais e componentes iniciais
    * integra animação de transição entre páginas (swup)

---
+++ src/frontend/scripts/animations.js
@@ -0,0 +1,7 @@
+import Swup from 'https://unpkg.com/swup@4?module'
+import SwupHeadPlugin from 'https://unpkg.com/@swup/head-plugin@2?module'
+
+const swup = new Swup({
+  plugins: [new SwupHeadPlugin({
+  })]
+})
