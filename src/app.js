51515b1c6e222cd6c51f958cc7484944be252e77
Pedro Pimentel Rodrigues <pedropimentelrdgs@gmail.com>
2026-04-21 21:26:51 -0300

    integra rotas de cadastro e perfil no app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
+const cadastroRoutes = require('./backend/routes/cadastroRoutes')
+const perfilRoutes = require('./backend/routes/perfilRoutes')
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'frontend')))
+
+// ROTAS BACKEND
+app.use('/api', cadastroRoutes)
+app.use('/api', perfilRoutes)
 
 module.exports = app
